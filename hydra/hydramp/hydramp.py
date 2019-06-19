85e3635fe96a155a5f14f80d35a892c3fd169211
devworks8 <devworks8@gmail.com>
2019-06-19 13:33:08 -0400

    Removed redundant code.

--- hydra/hydramp/hydramp.py
+++ hydra/hydramp/hydramp.py
@@ -94,38 +94,12 @@ class HydraMessenger(Process):
             if message:
                 self.validate_message(msg=message)
 
-        results = MessengerWorker(reporter=self.reporter, msg=worker.recv_multipart())
+        # results = HydraMessengerWorker(reporter=self.reporter, msg=message)
 
-        worker.send_multipart(results.validate_message(results.message))
-        print("Worker %s sending results" % self.identity)
-        print(results)
+            worker.send_multipart(self.validate_message(message))
+            print("Worker %s sending results" % self.identity)
+            print(message)
 
-    class HydraMessageWorker(Process):
-        def __init__(self, reporter, server=None, backend_port=None):
-            super().__init__()
-            self.settings = CfgManager()
-            self.command_message = CommandMessage()
-            self.identity = "%04X-%04X" % (randint(0, 0x10000), randint(0, 0x10000))
-            self.backend_host = self.settings.get('service_proxy_backend_host')[1]
-            self.backend_port = self.settings.get('service_proxy_backend_port')[1]
-            self.crypto = self.settings.get('service_messenger_crypto')[1]
-            self.reporter = reporter
-
-        def run(self):
-            context = zmq.Context()
-            worker = context.socket(zmq.REP)
-
-            try:
-                worker.connect("tcp://{host}:{port}".format(host=self.backend_host, port=self.backend_port))
-
-            except:
-
-
-            while True:
-                message = worker.recv_multipart()
-                worker.send_multipart(message)
-                print("Worker %s sending results" % self.identity)
-                print(message)
 
 processes = []
 
